48cba519289213c65285f10c5748c2ffd93a6dfe
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2022-08-25 17:37:17 -0500

    Uploading updated Bank Manager Program

--- BankManager.java
+++ BankManager.java
@@ -1,16 +1,20 @@
 //package BankManager;
+import java.util.Scanner;
 
 public class BankManager {
 	static void Bank() {
 		System.out.println("Welcome to Seamless Bank! Our repution is that we provide a seamless experience in many different transactions and creating various accounts for our clients!");
+		System.out.println();
+		System.out.println("What is your name?");
+		Scanner scan1 = new Scanner(System.in);
+		String name = scan1.nextLine();
+		System.out.println();
+		System.out.print("Welcome ");
+		System.out.println(name);
 	}
 
 	public static void main(String[] args) {
 		Bank();
-
 	}
-
 }
 
-
- 
