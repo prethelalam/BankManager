d79309ec11491c9200e26cd7ccb1693f363d7802
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2022-08-25 17:01:58 -0500

    Uploading Bank Manager Program

---
+++ BankManager.java
@@ -0,0 +1,16 @@
+//package BankManager;
+
+public class BankManager {
+	static void Bank() {
+		System.out.println("Welcome to Seamless Bank! Our repution is that we provide a seamless experience in many different transactions and creating various accounts for our clients!");
+	}
+
+	public static void main(String[] args) {
+		Bank();
+
+	}
+
+}
+
+
+ 
