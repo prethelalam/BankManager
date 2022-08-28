65d8a9411ccd79302b1c6cccdae1fcae502706e3
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2022-08-28 16:01:01 -0500

    Uploading updated Bank Manager Program

--- BankManager.java
+++ BankManager.java
@@ -21,33 +21,112 @@ public class BankManager {
 	}
 
 	static void Checking() {
-		System.out.println("Would you like to open a checking account? Enter y/n.");
+		System.out.println("Are you a new customer? (enter y/n)");
+		Scanner scan7 = new Scanner(System.in);
+		String answer = scan7.nextLine();
+		char Answer = answer.charAt(0);
+		System.out.println();
+
+		/*System.out.println("Would you like to open a checking account? Enter y/n.");
 		Scanner scan2 = new Scanner(System.in);
 		String option = scan2.nextLine();
 		char decision = option.charAt(0); //this converts the string into a char
-		System.out.println();
+		System.out.println();*/
+		
 
-		if (decision == 'y'){ //store this infomation in a text file. get/store customer age, sex.
+		if (Answer == 'y'){ //store this infomation in a text file. get/store customer age, sex, how much money they would like to deposit
+			System.out.println("Would you like to open a checking account? Enter y/n.");
+			Scanner scan2 = new Scanner(System.in);
+			String option = scan2.nextLine();
+			char decision = option.charAt(0); //this converts the string into a char
+			System.out.println();
+
+			//put option to make a savings account, maybe don't put it here 
+
 			System.out.println("You have chosen to create a checking account! At this moment we will take some of your information.");
 			System.out.println();
+
 			System.out.println("Please enter the name you would like your account to be under.");
 			Scanner scan3 = new Scanner(System.in);
 			String CustomerName = scan3.nextLine();
 			System.out.println();
-			System.out.print("The checking account will be under ");
-			System.out.println(CustomerName);
+
+			//System.out.print("The checking account will be under ");
+			//System.out.println(CustomerName); PUT AT THE END FOR SUMMARY OF INFORMATION THEN BE LIKE INFO HAS BEEN STORED (maybe)
+			//System.out.println();
+			System.out.println("What is your current age?");
+			Scanner scan4 = new Scanner(System.in);
+			String age = scan4.nextLine();
+			Integer Age = Integer.parseInt(age);
+			System.out.println();
+
+			System.out.println("What is your sex (M/F)?");
+			Scanner scan5 = new Scanner(System.in);
+			String sex = scan5.nextLine();
+			char Sex = sex.charAt(0);
+			System.out.println();
+
+			System.out.println("How much money would you like to deposit into your checking account?");
+			Scanner scan6 = new Scanner(System.in);
+			String amount = scan6.nextLine();
+			Double Deposit = Double.parseDouble(amount);
+			System.out.println();
+
+			try {
+				File CheckingInformation = new File ("CustomerCheckingAccount.txt");
+
+				if (!CheckingInformation.exists()){
+					CheckingInformation.createNewFile();
+				}
+				PrintWriter WriteCheckingInformation = new PrintWriter(CheckingInformation);
+				WriteCheckingInformation.println("The customers checking out information:");
+				WriteCheckingInformation.println();
+				WriteCheckingInformation.print("Customer name is ");
+				WriteCheckingInformation.println(CustomerName);
+				WriteCheckingInformation.print("Customer age is ");
+				WriteCheckingInformation.println(Age);
+				WriteCheckingInformation.print("Customers sex is ");
+				WriteCheckingInformation.println(Sex);
+				WriteCheckingInformation.print("Customers current balance: $");
+				WriteCheckingInformation.println(Deposit);
+				//other information asked by customer
+				WriteCheckingInformation.close();
+				System.out.println("Your information has been successfully saved!");
+			}
+			catch (IOException e){
+				e.printStackTrace();
+			}
+
 		}
-		else if (decision == 'n'){
+
+		else if (Answer == 'n'){
+			System.out.println("Would you like to see how much is in your checking balance? (enter y/n)");
+			Scanner scan8 = new Scanner(System.in);
+			String response = scan8.nextLine();
+			char Response = response.charAt(0);
+
+			if (Response == 'y'){
+				
+			}
+			//add another for saving balance?
+		}
+		
+		/*else if (decision == 'n'){
 			System.out.println();
 			System.out.println("You have opted into not creating a checking account."); //maybe add something here before welcome function
 			Welcome();
-		}
+		} */
+		
 	}
 
 	public static void main(String[] args) {
 		Welcome();
 		Checking();
+	} 
+}
 
+
+/*
 		try { //implement this in the checking function
 		File CheckingInformation = new File("CustomerCheckingInformation.txt"); // so we name the file then we create a new file and named it CustomerCheckingInformation
 
@@ -61,7 +140,4 @@ public class BankManager {
 		}
 		catch (IOException e){
 			e.printStackTrace(); //this is a try catch block
-		}
-	}
-}
-
+		} */
